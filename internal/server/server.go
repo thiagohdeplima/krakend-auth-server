8151d4466514278aaf63153b70ab945080a1b262
Thiago Henrique <thiagohdeplima@gmail.com>
2023-03-22 20:27:24 -0300

    feat: support HTTP connection

---
+++ internal/server/server.go
@@ -0,0 +1,37 @@
+package server
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/thiagohdeplima/krakend-auth-server/internal/usecase"
+)
+
+const TOKEN_PATH = "/token"
+
+type HTTPServer interface {
+	Run(http.ResponseWriter, *http.Request)
+}
+
+type Server struct {
+	usecase usecase.IssueToken
+	handler http.Handler
+}
+
+func NewServer(uc usecase.IssueToken, handler http.Handler) HTTPServer {
+	return &Server{usecase: uc, handler: handler}
+}
+
+func (s *Server) Run(w http.ResponseWriter, req *http.Request) {
+	if req.URL.Path != TOKEN_PATH {
+		s.handler.ServeHTTP(w, req)
+		return
+	}
+
+	s.serveToken(w, req)
+}
+
+func (s *Server) serveToken(w http.ResponseWriter, req *http.Request) {
+
+	fmt.Fprintf(w, "%q requested", req.URL.Path)
+}
--- main.go
+++ main.go
@@ -1,3 +1,40 @@
 package main
 
+import (
+	"context"
+	"net/http"
+
+	"github.com/thiagohdeplima/krakend-auth-server/internal/authenticator"
+	"github.com/thiagohdeplima/krakend-auth-server/internal/issuer"
+	"github.com/thiagohdeplima/krakend-auth-server/internal/repository"
+	"github.com/thiagohdeplima/krakend-auth-server/internal/server"
+	"github.com/thiagohdeplima/krakend-auth-server/internal/usecase"
+)
+
+type registerer string
+
+var (
+	pluginName        = "authorization-server"
+	HandlerRegisterer = registerer(pluginName)
+)
+
+func (r registerer) RegisterHandlers(f func(
+	string,
+	func(context.Context, map[string]interface{}, http.Handler) (http.Handler, error),
+)) {
+	f(string(r), r.registerHandlers)
+}
+
+func (r registerer) registerHandlers(_ context.Context, extra map[string]interface{}, handler http.Handler) (http.Handler, error) {
+	var rep = repository.NewRepository()
+
+	var val = authenticator.NewAuthenticator(rep)
+	var iss = issuer.NewTokenEmissor(rep)
+
+	var uc = usecase.NewTokenIssuer(val, iss)
+	var srv = server.NewServer(uc, handler)
+
+	return http.HandlerFunc(srv.Run), nil
+}
+
 func main() {}
