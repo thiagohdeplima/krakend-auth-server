44c41e0913fb8463c49f188574f009a6d5ea23e3
Thiago Henrique <thiagohdeplima@gmail.com>
2023-03-22 20:26:24 -0300

    chore: implement a mock repository

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,1 +1,20 @@
 package repository
+
+import "os"
+
+type Repository struct{}
+
+func NewRepository() *Repository {
+	return &Repository{}
+}
+
+func (r *Repository) GetSecretByClientID(string) (string, error) {
+	return "abc123", nil
+}
+
+func (r *Repository) GetKeypairByClientID(string) (privkey, pubkey string, _ error) {
+	priKey, _ := os.ReadFile("../../test/data/id_ecdsa")
+	pubKey, _ := os.ReadFile("../../test/data/id_ecdsa.pub")
+
+	return string(priKey), string(pubKey), nil
+}
