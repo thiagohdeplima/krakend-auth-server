a448bdade7c2e5ca5d29a719b7e27b745bc9e341
Thiago Henrique <thiagohdeplima@gmail.com>
2023-03-27 17:15:07 -0300

    styles: remove useless fmt

--- internal/repo/error.go
+++ internal/repo/error.go
@@ -1,11 +1,7 @@
 package repo
 
-import (
-	"fmt"
-)
-
 type KeyNotFoundError struct{}
 
 func (e *KeyNotFoundError) Error() string {
-	return fmt.Sprint("required key not found")
+	return "required key not found"
 }
